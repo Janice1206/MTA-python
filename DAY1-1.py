3e5279dc33a162582c66bfa487fcb71d91374525
Janice1206 <77949836+Janice1206@users.noreply.github.com>
2021-01-25 11:58:57 +0800

    梯形

---
+++ DAY1-1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 25 10:01:45 2021
+
+@author: MTAEXA
+
+print(10)
+
+name=input("name?")
+printlk("hello",name)
+"""
+up=float(input("上底?"))
+down=float(input("下底?"))
+h=float(input("高?"))
+print((up+down)*h/2)
+
+        
